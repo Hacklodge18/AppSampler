376a75052adeb075e6e44491e060ae7ce7d32805
Matthew Gong <Matthewzgong@gmail.com>
2018-12-17 13:10:28 -0800

    Buttons

--- app/src/main/java/com/hacklodge/seattle/appsampler/Platter.java
+++ app/src/main/java/com/hacklodge/seattle/appsampler/Platter.java
@@ -7,11 +7,46 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.content.Intent;
+import android.net.Uri;
+
 
 public class Platter extends AppCompatActivity {
-
+    InstalledAppsManager Manager;
+    AppHolder app1 = new AppHolder("com.supercell.brawlstars","BrawlStar" , null);
+    AppHolder app3 = new AppHolder("com.lemonjamstudio.infiniteknights","InfiniteKnight" , null);
+    AppHolder app2 = new AppHolder("com.mochibits.wordtoword.google","wordtoword" , null);
+    AppHolder app4 = new AppHolder("om.pinestreetcodeworks.TinyBubbles","TinnyBubbles" , null);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_platter);
+        Button appB1 = (Button)findViewById(R.id.app1);
+        appB1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InstallUtility.install(view.getContext() , app1 , Manager);
+            }
+        });
+        Button appB2 = (Button)findViewById(R.id.app2);
+        appB2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InstallUtility.install(view.getContext() , app2 , Manager);
+            }
+        });
+        Button appB3 = (Button)findViewById(R.id.app3);
+        appB3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InstallUtility.install(view.getContext() , app3 , Manager);
+            }
+        });
+        Button appB4 = (Button)findViewById(R.id.app4);
+        appB4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InstallUtility.install(view.getContext() , app4 , Manager);
+            }
+        });
     }
 }
