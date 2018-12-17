bb89bbd53c2cf1f0487bd72ca58c5077b259e30d
NikolasUntoten <nikolasr@ggaub.com>
2018-12-16 21:48:57 -0800

    Initial commit

---
+++ app/src/main/java/com/hacklodge/seattle/appsampler/Platter.java
@@ -0,0 +1,13 @@
+package com.hacklodge.seattle.appsampler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Platter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_platter);
+    }
+}
