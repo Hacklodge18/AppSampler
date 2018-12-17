edd19af2ca9ae124c7487c316e2ed316f5f11f0c
Matthew Gong <Matthewzgong@gmail.com>
2018-12-17 10:49:20 -0800

    Nothing just a test

--- app/src/main/java/com/hacklodge/seattle/appsampler/Platter.java
+++ app/src/main/java/com/hacklodge/seattle/appsampler/Platter.java
@@ -2,12 +2,16 @@ package com.hacklodge.seattle.appsampler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class Platter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_platter);
+
     }
 }
